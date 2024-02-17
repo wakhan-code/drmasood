b4d9c272618c2433432b589f19cd794ce09183a1
wakhan-code <159034227+wakhan-code@users.noreply.github.com>
2024-02-17 08:22:59 -0800

    commit

--- src/assets/js/dark.js
+++ src/assets/js/dark.js
@@ -15,7 +15,7 @@ function disableDarkMode() {
 // determines a new users dark mode preferences
 function detectColorScheme() {
 	// default to the light theme
-	let theme = 'dark';
+	let theme = 'light';
 
 	// check localStorage for a saved 'theme' variable. if it's there, the user has visited before, so apply the necessary theme choices
 	if (localStorage.getItem('theme')) {
@@ -23,7 +23,7 @@ function detectColorScheme() {
 	}
 	// if it's not there, check to see if the user has applied dark mode preferences themselves in the browser
 	else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-		theme = 'light';
+		theme = 'dark';
 	}
 
 	// if there is no preference set, the default of light will be used. apply accordingly
